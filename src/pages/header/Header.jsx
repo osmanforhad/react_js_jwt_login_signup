05dccd3e8df7375553b83e96735f7dc259eb649b
osmanforhad <osmanforhad93@gmail.com>
2025-04-01 00:08:41 +0600

    setup navigation bar as a header

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Navbar, Container, Nav} from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand><strong>Not LoggedIn</strong></Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/login" className="nav-link">Login</Nav.Link>
+            <Nav.Link as={Link} to="/register" className="nav-link">Signup</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
