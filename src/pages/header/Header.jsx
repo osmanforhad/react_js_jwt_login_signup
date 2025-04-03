b1518b1d43a835068250f3ccbde679650d8e049f
osmanforhad <osmanforhad93@gmail.com>
2025-04-04 04:42:03 +0600

    setup logout functionality and remove token from local storage

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import {Navbar, Container, Nav} from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./Header.css";
 
 const Header = () => {
+  //get the logged in user token from local storage
+  const token = localStorage.getItem("token");
+  //initialize the navigation hook
+  const navigate = useNavigate();
+  //method for working with logged out functionality
+  const handleLogout =() => {
+    //remove token from local storage
+    localStorage.removeItem("token");
+    //navigate to login component
+    navigate("/login");
+  }
   return (
     <>
-    <Navbar bg="dark" data-bs-theme="dark">
+    <Navbar bg={token ? "primary" : "dark"} variant="dark">
         <Container>
-          <Navbar.Brand><strong>Not LoggedIn</strong></Navbar.Brand>
+          <Navbar.Brand>
+            <strong>{token ? "Logged-In" : "Not-LoggedIn"}</strong>
+          </Navbar.Brand>
           <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/login" className="nav-link">Login</Nav.Link>
-            <Nav.Link as={Link} to="/register" className="nav-link">Signup</Nav.Link>
+            {token ? (<>
+              <Nav.Link as={Link} to="/dashboard" className="nav-link">Dashboard</Nav.Link>
+              <Nav.Link className="nav-link" onClick={handleLogout}>logout</Nav.Link>
+            </>) :(<>
+              <Nav.Link as={Link} to="/login" className="nav-link">Login</Nav.Link>
+              <Nav.Link as={Link} to="/register" className="nav-link">Signup</Nav.Link>
+            </>)}
           </Nav>
         </Container>
       </Navbar>
